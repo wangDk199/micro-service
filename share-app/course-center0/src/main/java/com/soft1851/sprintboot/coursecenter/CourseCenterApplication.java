f378f24ac0cccb6d33906839518b73977563b480
wangDk199 <1091886414@qq.com>
2020-09-22 16:02:47 +0800

    项目框架

---
+++ share-app/course-center0/src/main/java/com/soft1851/sprintboot/coursecenter/CourseCenterApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.sprintboot.coursecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseCenterApplication.class, args);
+    }
+
+}
